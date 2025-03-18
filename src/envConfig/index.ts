69ac6df59a3fec8f251e535ea84606ebc83ba6e4
md-Rafiqulislam <greenmate919@gmail.com>
2025-03-18 22:53:46 +0600

    add more env file.

--- src/envConfig/index.ts
+++ src/envConfig/index.ts
@@ -11,4 +11,8 @@ export const envFile = {
     port: process.env.PORT,
     dbUrl: process.env.DATABASE_URL as string,
     nEnv: process.env.NODE_ENV,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET as string,
+    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET as string,
+    accessTokenExpire: process.env.ACCESS_TOKEN_EXPIRE_IN as string,
+    refreshTokenExpire: process.env.REFRESH_TOKEN_EXPIRE_IN as string,
 };
